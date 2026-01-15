41a96a0e0e9f408a68cb145ed2dd2f68cd4f4c53
Cole Neville <5910739+ColeNeville@users.noreply.github.com>
2026-01-15 09:34:13 -0700

    fix: remove project.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Remove default Shift-H and Shift-L which are moving tabs
+vim.keymap.del({ "n" }, "H")
+vim.keymap.del({ "n" }, "L")
+
+local map = LazyVim.safe_keymap_set
+
+map({ "i", "n", "v" }, "<C-Left>", "<CMD>normal! b<CR>", { desc = "Previous word" })
+map({ "i", "n", "v" }, "<C-Right>", "<CMD>normal! e<CR>", { desc = "Next word" })
+
+map({ "n" }, "<C-Down>", "10<Down>")
+map({ "n" }, "<C-Up>", "10<Up>")
+map({ "n" }, "<C-,>", "<CMD>resize -2<CR>", { desc = "Shrink window" })
+map({ "n" }, "<C-.>", "<CMD>resize +2<CR>", { desc = "Expand window" })
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -5,4 +5,15 @@ return {
       dashboard = { enabled = false },
     },
   },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        {
+          mode = { "n", "x" },
+          { "<leader>p", group = "[P]rojects", icon = " " },
+        },
+      },
+    },
+  },
 }
